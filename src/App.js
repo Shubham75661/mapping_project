e454afb175dd79e8e0473191df1e2e36fba9bae4
shubham <shubham75661@gmail.com>
2022-08-28 13:26:37 +0530

    Added react-router and created map componet

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import logo from './logo.svg';
+import { BrowserRouter, Routes, Route  } from "react-router-dom"
+import Map from './components/Map';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>it is working</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path = "/" element = {<Map/>}/>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
